fb18c92472ec0cf0a214565ade4b85853d31545c
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-10 20:49:07 -0500

    added store in index.js

--- client/index.js
+++ client/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./app/App";
+import store from "./app/store";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
