9694498acb6ba9a2669e4ba9759c75f4921fa18c
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-10 21:55:16 -0500

    added generateProjSlice in the reducer

--- client/app/store.js
+++ client/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dependencySlice from "../features/dependencySlice";
+import generateProjSlice from "../features/generateProjSlice";
 
 const store = configureStore({
   reducer: {
     dependency: dependencySlice,
+    project: generateProjSlice,
   },
 });
 export default store;
