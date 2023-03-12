548012b8e6d8651f523e129e8409c3eaf5115540
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-11 21:47:02 -0500

    updated sample index file

--- public/sampleIndex.js
+++ public/sampleIndex.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
+root.render(<App />);
